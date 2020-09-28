24c0c04e88c52bbdb512e9164b9580e34b98497e
passiongreen <67631747+passiongreen@users.noreply.github.com>
2020-09-28 17:56:54 +0900

    Create Game1if.java

---
+++ Game1if.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    boolean condition = true;
+
+    if (condition){
+      System.out.println("조건은 참!");
+    }else{
+      System.out.println("조건은 거짓!");
+    }
+  }
+}
