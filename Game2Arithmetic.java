52dc9c2c0cb15ca4717d20c3689fa78015312d8b
passiongreen <67631747+passiongreen@users.noreply.github.com>
2020-09-28 18:07:09 +0900

    Create Game2Arithmetic.java

---
+++ Game2Arithmetic.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int num = 256;
+
+    System.out.println("주어진  수 : " + num);
+    System.out.println("  1의 자리 : " + num % 10);
+    num = num/10;
+    System.out.println(" 10의 자리 : " + num % 10);
+    num = num/10;
+    System.out.println("100의 자리 : " + num % 10);
+  }
+}
