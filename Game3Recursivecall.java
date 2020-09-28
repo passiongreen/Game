2242ff57631ad06b52c6c04dd12b932fa2ddb6a0
passiongreen <67631747+passiongreen@users.noreply.github.com>
2020-09-28 18:16:34 +0900

    Create Game3Recursivecall.java

---
+++ Game3Recursivecall.java
@@ -0,0 +1,32 @@
+class Main {
+  //                          // 2     1
+  // public static int power(int x, int y)
+  // {
+  //   if(y<=0){ 
+  //     return 1;
+  //   }else{
+  //     return x * power(x, y-1); // x를 y번 곱함
+  //   }
+  // }
+
+
+  //while
+
+    public static int power(int x, int y)
+    {
+      while(조건식) {
+
+
+
+      } return sum;
+    }
+
+  public static void main(String[] args)
+  {
+    System.out.println("power(2,1) : "+ power(2,1));
+    System.out.println("power(2,2) : "+ power(2,2));
+    System.out.println("power(2,3) : "+ power(2,3));
+    System.out.println("power(2,4) : "+ power(2,4));
+    System.out.println("power(2,5) : "+ power(2,5));
+  }
+}
