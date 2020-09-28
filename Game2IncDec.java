e1c941e3fff21a3254306a45bde93a40dcc1b370
passiongreen <67631747+passiongreen@users.noreply.github.com>
2020-09-28 18:07:36 +0900

    Create Game2IncDec.java

---
+++ Game2IncDec.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args)
+  {
+
+//     
+
+    int a = 10;
+    int b = 10;
+  
+
+     int x = a++;  // a = 11 , x = 10
+     int y = ++b;  // b = 11 , y = 11
+    System.out.println("x = "+ x +", y = "+ y +", a = "+ a + ", b = " + b);
+
+
+
+   // System.out.println("x = "+ x +", y = "+ y +", z = "+ z);
+  }
+}
+
+
+// int   =  10
